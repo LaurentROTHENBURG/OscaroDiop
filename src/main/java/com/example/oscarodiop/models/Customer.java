6f8575979772e0a863ba7f16ba199ea3de43fd88
Thioro <dthioro@yahoo.fr>
2021-06-10 17:27:38 +0200

    modif faite sur model

--- src/main/java/com/example/oscarodiop/models/Customer.java
+++ src/main/java/com/example/oscarodiop/models/Customer.java
@@ -1,4 +1,105 @@
 package com.example.oscarodiop.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "customer_id_seq")
+    @SequenceGenerator(name="customer_id_seq",allocationSize = 1)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String mail;
+    private String user;
+    private String password;
+
+    @JsonIgnore
+     @OneToMany(mappedBy ="order")
+   private List<Order> orderList;
+
+    public Customer(Long id, String firstName, String lastName, String phoneNumber, String mail, String user, String password, List<Order> orderList) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.mail = mail;
+        this.user = user;
+        this.password = password;
+        this.orderList = orderList;
+    }
+
+
+    public Customer(){}
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Order> getOrderList() {
+        return orderList;
+    }
+
+    public void setOrderList(List<Order> orderList) {
+        this.orderList = orderList;
+    }
 }
--- src/main/java/com/example/oscarodiop/models/OrderLine.java
+++ src/main/java/com/example/oscarodiop/models/OrderLine.java
@@ -1,4 +1,9 @@
 package com.example.oscarodiop.models;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
 public class OrderLine {
+
+
 }
