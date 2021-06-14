8416c5d9eb5b04a66f2104aa5991b88e9c80550b
Thioro <dthioro@yahoo.fr>
2021-06-14 14:45:16 +0200

    mise à jour du fichier SQL

--- src/main/java/com/example/oscarodiop/controllers/PartGroupController.java
+++ src/main/java/com/example/oscarodiop/controllers/PartGroupController.java
@@ -2,10 +2,14 @@ package com.example.oscarodiop.controllers;
 
 import com.example.oscarodiop.models.PartGroup;
 import com.example.oscarodiop.repositories.PartGroupRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @RestController
@@ -22,4 +26,24 @@ public class PartGroupController {
     public List<PartGroup> getAllPartGroup(){
         return partGroupRepository.findAll();
     }
+
+    @PostMapping
+    public Long createPartGroupe(@RequestParam PartGroup partGroup){
+      return   partGroupRepository.save(partGroup).getId();
+    }
+@Transactional
+    @DeleteMapping("/{id}")
+    public void deletePartGroupe(@PathVariable Long id){
+        partGroupRepository.deleteById(id.intValue());
+    }
+
+    @Transactional //pour que la methode modifie la base
+    @PutMapping("/{id}/value")
+    public ResponseEntity updatePartGroup(@PathVariable Long id, @RequestBody String value){
+        return new ResponseEntity((partGroupRepository.setValueById(value,id)!=0)
+    ? HttpStatus.OK : HttpStatus.NOT_FOUND);
+    }
+
+
+
 }
